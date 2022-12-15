76374c1b70d374005084dc5a2114bf2a5e8f7642
jong hun pak <fireking5997@gmail.com>
2022-12-15 22:55:30 +0900

    add:route test with json parse

--- reactTest/src/components/tests/VideoCard.test.js
+++ reactTest/src/components/tests/VideoCard.test.js
@@ -1,46 +1,64 @@
-// import { render, screen } from '@testing-library/react';
-// import { MemoryRouter } from 'react-router-dom';
-// import { formatAgo } from '../../util/date';
-// import VideoCard from '../VideoCard';
-
+import React from "react";
+import { MemoryRouter, Routes, Route, useLocation } from "react-router-dom";
+import { formatAgo } from "../../util/date";
+import VideoCard from "../VideoCard";
+import { video } from "./mockData";
+import userEvent from "@testing-library/user-event";
 const { render, screen } = require("@testing-library/react");
 
-// describe('VideoCard', () => {
-//   const video = {
-//     id: 1,
-//     snippet: {
-//       title: 'title',
-//       channelId: '1',
-//       channelTitle: 'channelTitle',
-//       publishedAt: new Date(),
-//       thumbnails: {
-//         medium: {
-//           url: 'http://image/',
-//         },
-//       },
-//     },
-//   };
-//   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+const {
+  id,
+  snippet: { channelTitle, publishedAt, thumbnails, title },
+} = video;
 
-//   it('renders video item', () => {
-//     render(
-//       <MemoryRouter>
-//         <VideoCard video={video} />
-//       </MemoryRouter>
-//     );
+describe("videoCard", () => {
+  it("존재 해야한다.", () => {
+    render(
+      <MemoryRouter>
+        <VideoCard video={video} />
+      </MemoryRouter>
+    );
+    const img = screen.getByRole("img");
+    expect(img).toBeInTheDocument();
+  });
 
-//     const image = screen.getByRole('img');
-//     expect(image.src).toBe(thumbnails.medium.url);
-//     expect(image.alt).toBe(title);
-//     expect(screen.getByText(title)).toBeInTheDocument();
-//     expect(screen.getByText(channelTitle)).toBeInTheDocument();
-//     expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
-//   });
-// });
+  it("속성이 데이터와 같아야 한다", () => {
+    render(
+      <MemoryRouter>
+        <VideoCard video={video} />
+      </MemoryRouter>
+    );
 
-describe("a", () => {
-  test("d", () => {
-    render(<img />);
-    screen.getByRole("img");
+    const img = screen.getByRole("img");
+    expect(img.src).toBe(thumbnails.medium.url);
+    expect(img.alt).toBe(title);
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(channelTitle)).toBeInTheDocument();
+    expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+
+  it("카드 클릭시 디테일 페이지로 비디오 데이터와 함께 이동한다", () => {
+    function LocationStatDisplay() {
+      console.log(JSON.stringify(useLocation().state));
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path='/' element={<VideoCard video={video} />} />
+          <Route
+            path={`/videos/watch/${video.id}`}
+            element={<LocationStatDisplay />}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const li = screen.getByRole("listitem");
+    userEvent.click(li);
+
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
   });
 });
---
+++ reactTest/src/components/tests/mockData.js
@@ -0,0 +1,17 @@
+export const video = {
+  id: 1,
+  snippet: {
+    channelTitle: "aa",
+    publishedAt: "2022-09-24T19:59:59Z",
+    channelId: "UCX6OQ3DkcsbYNE6H8uQQuVA",
+    title: "Survive 100 Days In Circle, Win $500,000",
+    description: "sign",
+    thumbnails: {
+      medium: {
+        url: "https://i.ytimg.com/vi/gHzuabZUd6c/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+    },
+  },
+};
