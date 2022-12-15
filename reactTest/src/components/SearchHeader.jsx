0ab09e6888c3338562721edd273039d78d49f685
jong hun pak <fireking5997@gmail.com>
2022-12-16 01:47:57 +0900

    add:searchHeader 테스트

--- reactTest/src/components/SearchHeader.jsx
+++ reactTest/src/components/SearchHeader.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { BsYoutube, BsSearch } from 'react-icons/bs';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { BsYoutube, BsSearch } from "react-icons/bs";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function SearchHeader() {
   const { keyword } = useParams();
   const navigate = useNavigate();
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/videos/${text}`);
   };
 
-  useEffect(() => setText(keyword || ''), [keyword]);
+  useEffect(() => setText(keyword || ""), [keyword]);
 
   return (
     <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <h1>{text}</h1>
       <Link to='/' className='flex items-center'>
         <BsYoutube className='text-4xl text-brand' />
         <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
--- reactTest/src/components/tests/searchHeader.test.jsx
+++ reactTest/src/components/tests/searchHeader.test.jsx
@@ -4,8 +4,17 @@ import { Route } from "react-router-dom";
 import { withRoute } from "../../tests/utils";
 import SearchHeader from "../SearchHeader";
 import userEvent from "@testing-library/user-event";
+import renderer from "react-test-renderer";
 
 describe("searchCard", () => {
+  it("snapShot", () => {
+    const tree = renderer
+      .create(withRoute(<Route path='/' element={<SearchHeader />} />))
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
   it("랜더링 된 링크태그가 존재한다", () => {
     render(withRoute(<Route path='/' element={<SearchHeader />} />));
     expect(screen.getByText("Youtube")).toBeInTheDocument();
@@ -49,5 +58,43 @@ describe("searchCard", () => {
     const button = screen.getByRole("button");
     userEvent.click(button);
     expect(screen.getByRole("heading", { name: "Text" })).toBeInTheDocument();
+  });
+
+  it("경로가 제대로 입력된 파라미터 대로 반영 된다", () => {
+    render(
+      withRoute(
+        <>
+          <Route path='/:keyword' element={<SearchHeader />} />
+        </>,
+        ["/bts"]
+      )
+    );
+
+    expect(screen.getByDisplayValue("bts")).toBeInTheDocument();
+  });
+
+  it("인풋에 입력된 파라미터 대로 반영 된다", () => {
+    const term = "abc";
+
+    render(
+      withRoute(
+        <>
+          <Route path='/:keyword' element={<SearchHeader />} />
+          <Route path={`/videos/${term}`} element={<h1>{term}</h1>} />
+        </>,
+        ["/home"]
+      )
+    );
+
+    expect(screen.getByDisplayValue("home")).toBeInTheDocument();
+    const button = screen.getByRole("button");
+    const input = screen.getByRole("textbox");
+    userEvent.clear(input);
+    userEvent.type(input, term);
+    expect(screen.getByText(term)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(term)).toBeInTheDocument();
+    userEvent.click(button);
+
+    expect(screen.getByText(term)).toBeInTheDocument();
   });
 });
